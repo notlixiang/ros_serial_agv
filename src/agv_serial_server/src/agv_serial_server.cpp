81fd637ee6812c5471b7f3058a38a6d6560b7782
null_me_b3 <lixiang3.1416@qq.com>
2019-03-19 13:56:10 +0800

    add previous project code

--- src/agv_serial_server/src/agv_serial_server.cpp
+++ src/agv_serial_server/src/agv_serial_server.cpp
@@ -7,23 +7,14 @@
 #include <std_msgs/Float32.h>
 #include <std_msgs/String.h>
 #include <string>
+#include "geometry_msgs/Twist.h"
 
 #include <StructSerial.h>
 
-#define USHORT unsigned short int
-#define BYTE unsigned char
-#define CHAR char
-#define UCHAR unsigned char
-#define UINT unsigned int
-#define DWORD unsigned int
-#define PVOID void *
-#define ULONG unsigned int
-#define INT int
-#define UINT32 UINT
-#define LPVOID void *
-#define BOOL BYTE
-#define TRUE 1
-#define FALSE 0
+#define FREQUENCY 10
+#define COMMAND_SIZE 11 
+ 
+#define MAXZERONUM 5
 
 serial::Serial ser;
 using namespace std;
@@ -32,6 +23,103 @@ float speed_cmd[3]={0};
 float pos_cmd[3]={0};
 uint8_t  qr_scan_cmd=0;
 
+class DataUpdater
+{
+public:
+    DataUpdater(ros::NodeHandle &nh);
+    ~DataUpdater();
+    geometry_msgs::Twist getCarVel();
+
+private:
+    // callback functions
+    void carVelCallback(const geometry_msgs::Twist::ConstPtr& msg);
+
+    // subscribers
+    ros::Subscriber carVelSub;
+
+    // data
+    geometry_msgs::Twist::ConstPtr carVel; //boost::shared_ptr<const geometry_msgs::Twist_<std::allocator<void> > > 
+    // the latest time that recieve a message
+    ros::Time carVelTime;
+    int ACCEPTTIME;
+};
+
+DataUpdater::DataUpdater(ros::NodeHandle &nh)
+{
+    carVelSub = nh.subscribe("cmd_vel",1,&DataUpdater::carVelCallback,this);
+    ACCEPTTIME = 500000000; // 0.5s, unit: ns(10^-9s) TODO: this may need change
+}
+
+DataUpdater::~DataUpdater()
+{
+}
+
+void DataUpdater::carVelCallback(const geometry_msgs::Twist::ConstPtr& msg)
+{
+    carVel = msg;
+    //ROS_INFO("car_vel: %.4f, %.4f, %.4f", msg->linear.x, msg->linear.y, msg->angular.z);
+    carVelTime = ros::Time::now();
+}
+
+geometry_msgs::Twist DataUpdater::getCarVel()
+{
+    ros::Time nowTime = ros::Time::now();
+    ros::Duration timediff = nowTime-carVelTime;
+    if(carVel && timediff.nsec<ACCEPTTIME && timediff.sec==0 ) // this message is new enough
+        return *carVel;
+    geometry_msgs::Twist dummyZero;
+    //ROS_INFO("RETURN DUMMY");
+    return dummyZero;
+}
+
+void genCmd(char* cmdData, short vx, short vy, short va)
+{
+    void* tempP1 = &vx;
+    void* tempP2 = &vy;
+    void* tempP3 = &va;
+    char* velxChar = (char*)tempP1;
+    char* velyChar = (char*)tempP2;
+    char* velaChar = (char*)tempP3;
+
+    cmdData[0] = (char)0x53;
+    cmdData[1] = (char)0x4A;
+    cmdData[2] = (char)0x54;
+    cmdData[3] = (char)0x55;
+    cmdData[4] = velxChar[1]; //swap the high byte and the low byte
+    cmdData[5] = velxChar[0];
+    cmdData[6] = velyChar[1];
+    cmdData[7] = velyChar[0];
+    cmdData[8] = velaChar[1];
+    cmdData[9] = velaChar[0];
+    cmdData[10] = (char)0x01; //
+
+}
+
+void velFilter(short &vx, short &vy, short &va, int &count, short &lastvx, short &lastvy, short &lastva)
+{
+    if(vx==0 && vy==0 && va==0)
+    {
+        count++;
+        if(count>MAXZERONUM)
+        {
+            count = MAXZERONUM;
+        }
+        else
+        {
+            vx = lastvx;
+            vy = lastvy;
+            va = lastva;
+            ROS_INFO("*****FILT OUT********");
+        }
+    }
+    else
+    {
+        count = 0;
+        lastvx = vx;
+        lastvy = vy;
+        lastva = va;
+    }
+}
 
 int main(int argc, char **argv) {
     try {
