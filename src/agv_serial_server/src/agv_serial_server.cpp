b32aa4bee46936ce2e9529334b2e6b1119e8d4da
NULL-LI <lixiang3.1416@qq.com>
2019-12-07 22:54:57 +0800

    now tested

--- src/agv_serial_server/src/agv_serial_server.cpp
+++ src/agv_serial_server/src/agv_serial_server.cpp
@@ -5,6 +5,7 @@
 #include <std_msgs/Bool.h>
 #include <std_msgs/Empty.h>
 #include <std_msgs/Float32.h>
+#include <std_msgs/Int32.h>
 #include <std_msgs/Int32MultiArray.h>
 #include <std_msgs/String.h>
 #include <string>
@@ -24,22 +25,29 @@ float speed_cmd[3] = {0};
 float pos_cmd[3] = {0};
 uint8_t qr_scan_cmd = 0;
 
-const char *mystrstr(const char *pOri, int OriNum, const char *pFind, int FindNum) {
+const char *mystrstr(const char *pOri, int OriNum, const char *pFind, int FindNum)
+{
     // char *p = NULL;
     if (OriNum < FindNum)
         return NULL;
-    else {
+    else
+    {
         int i = 0, j = 0, Match = 0;
-        for (i = 0; i < OriNum && FindNum + i + 1 <= OriNum; i++) {
+        for (i = 0; i < OriNum && FindNum + i + 1 <= OriNum; i++)
+        {
             int e = i;
-            for (j = 0; j < FindNum; j++) {
-                if (!memcmp(pFind + j, pOri + e, 1)) {
+            for (j = 0; j < FindNum; j++)
+            {
+                if (!memcmp(pFind + j, pOri + e, 1))
+                {
                     e++;
                     Match++;
-                } else
+                }
+                else
                     Match = 0;
             }
-            if (Match == FindNum) {
+            if (Match == FindNum)
+            {
                 return (pOri + i);
                 break;
             }
@@ -49,7 +57,8 @@ const char *mystrstr(const char *pOri, int OriNum, const char *pFind, int FindNu
     return NULL;
 }
 
-class DataUpdater {
+class DataUpdater
+{
 public:
     DataUpdater(ros::NodeHandle &nh);
 
@@ -74,26 +83,31 @@ private:
     int ACCEPTTIME;
 };
 
-DataUpdater::DataUpdater(ros::NodeHandle &nh) {
+DataUpdater::DataUpdater(ros::NodeHandle &nh)
+{
     carVelSub = nh.subscribe("cmd_vel", 1, &DataUpdater::carVelCallback, this);
     carQrSub = nh.subscribe("cmd_qr", 1, &DataUpdater::carQrCallback, this);
     ACCEPTTIME = 500000000; // 0.5s, unit: ns(10^-9s) TODO: this may need change
 }
 
-DataUpdater::~DataUpdater() {
+DataUpdater::~DataUpdater()
+{
 }
 
-void DataUpdater::carVelCallback(const geometry_msgs::Twist::ConstPtr &msg) {
+void DataUpdater::carVelCallback(const geometry_msgs::Twist::ConstPtr &msg)
+{
     carVel = msg;
     //ROS_INFO("car_vel: %.4f, %.4f, %.4f", msg->linear.x, msg->linear.y, msg->angular.z);
     carVelTime = ros::Time::now();
 }
 
-void DataUpdater::carQrCallback(const std_msgs::Bool &msg) {
+void DataUpdater::carQrCallback(const std_msgs::Bool &msg)
+{
     qr_scan_cmd = 1;
 }
 
-geometry_msgs::Twist DataUpdater::getCarVel() {
+geometry_msgs::Twist DataUpdater::getCarVel()
+{
     ros::Time nowTime = ros::Time::now();
     ros::Duration timediff = nowTime - carVelTime;
     if (carVel && timediff.nsec < ACCEPTTIME && timediff.sec == 0) // this message is new enough
@@ -103,40 +117,47 @@ geometry_msgs::Twist DataUpdater::getCarVel() {
     return dummyZero;
 }
 
-void genCmd(char *cmdData, short vx, short vy, short va) {
+void genCmd(char *cmdData, short vx, short vy, short va)
+{
     void *tempP1 = &vx;
     void *tempP2 = &vy;
     void *tempP3 = &va;
-    char *velxChar = (char *) tempP1;
-    char *velyChar = (char *) tempP2;
-    char *velaChar = (char *) tempP3;
+    char *velxChar = (char *)tempP1;
+    char *velyChar = (char *)tempP2;
+    char *velaChar = (char *)tempP3;
 
-    cmdData[0] = (char) 0x53;
-    cmdData[1] = (char) 0x4A;
-    cmdData[2] = (char) 0x54;
-    cmdData[3] = (char) 0x55;
+    cmdData[0] = (char)0x53;
+    cmdData[1] = (char)0x4A;
+    cmdData[2] = (char)0x54;
+    cmdData[3] = (char)0x55;
     cmdData[4] = velxChar[1]; //swap the high byte and the low byte
     cmdData[5] = velxChar[0];
     cmdData[6] = velyChar[1];
     cmdData[7] = velyChar[0];
     cmdData[8] = velaChar[1];
     cmdData[9] = velaChar[0];
-    cmdData[10] = (char) 0x01; //
-
+    cmdData[10] = (char)0x01; //
 }
 
-void velFilter(float &vx, float &vy, float &va, int &count, float &lastvx, float &lastvy, float &lastva) {
-    if (vx == 0 && vy == 0 && va == 0) {
+void velFilter(float &vx, float &vy, float &va, int &count, float &lastvx, float &lastvy, float &lastva)
+{
+    if (vx == 0 && vy == 0 && va == 0)
+    {
         count++;
-        if (count > MAXZERONUM) {
+        if (count > MAXZERONUM)
+        {
             count = MAXZERONUM;
-        } else {
+        }
+        else
+        {
             vx = lastvx;
             vy = lastvy;
             va = lastva;
             // ROS_INFO("*****FILT OUT********");
         }
-    } else {
+    }
+    else
+    {
         count = 0;
         lastvx = vx;
         lastvy = vy;
@@ -144,21 +165,24 @@ void velFilter(float &vx, float &vy, float &va, int &count, float &lastvx, float
     }
 }
 
-bool send_struct_command_serial(serial::Serial &ser) {
+bool send_struct_command_serial(serial::Serial &ser)
+{
     struct_command_data command;
     struct_command_data *command_ptr = &command;
-    char *command_ptr_char = (char *) command_ptr;
+    char *command_ptr_char = (char *)command_ptr;
 
     uint8_t cmd_buff[150];
     int i = 0;
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < 3; i++)
+    {
         command.speed_cmd[i] = speed_cmd[i];
         command.pos_cmd[i] = pos_cmd[i];
     }
 
     command.qr_scan_cmd = qr_scan_cmd;
     ROS_INFO("qr_scan_cmd %d", qr_scan_cmd);
-    if (qr_scan_cmd != 0) {
+    if (qr_scan_cmd != 0)
+    {
         qr_scan_cmd = 0;
     }
     command.check_front_cmd = CHECK_FRONT_CMD;
@@ -189,179 +213,23 @@ bool send_struct_command_serial(serial::Serial &ser) {
     return true;
 }
 
-int main(int argc, char **argv) {
-    try {
-        ser.setPort("/dev/ttyUSB0");
-        ser.setBaudrate(115200);
-        serial::Timeout to = serial::Timeout::simpleTimeout(1000);
-        ser.setTimeout(to);
-        ser.open();
-    }
-    catch (serial::IOException &e) {
-        ROS_ERROR_STREAM("Unable to open port ");
-        return -1;
-    }
-    if (ser.isOpen()) {
-        ROS_INFO_STREAM("Serial Port initialized");
-    } else {
-        return -1;
-    }
+int main(int argc, char **argv)
+{
 
     ros::init(argc, argv, "agv_serial_server");
     ros::NodeHandle nh;
     DataUpdater dataUpdater(nh);
     ros::Rate loop_rate(FREQUENCY);
 
-    ros::Publisher infrared_states_publisher = nh.advertise<std_msgs::Int32MultiArray>("agv_infrared_states", 1000);
-    std_msgs::Int32MultiArray infrared_states_msg;
-
-    bool serialflag = true;
-    std::string datastr = "";
-    datastr.clear();
-    // char feedback_buff[200];
-    const char *front_fbk = "FBK";
-    const char *back_fbk = "fbk";
-    // printf("FEEDBACK_DATA_LENGTH %d\n",FEEDBACK_DATA_LENGTH);
-
-    geometry_msgs::Twist carVelocity;
-    int zeroCount = 0;
-    float lastvx = 0;
-    float lastvy = 0;
-    float lastva = 0;
-    float vx, vy, va;
-    float velx, vely, vela;
-    char cmd[COMMAND_SIZE + 1];
-    cmd[COMMAND_SIZE] = '\0';
-
+    ros::Publisher int_publisher = nh.advertise<std_msgs::Int32>("int_test", 1000);
+    std_msgs::Int32 int_msg;
+    int iter=0;
     bool flag = true;
-    while (ros::ok()) {
-
-        // flag = !flag;
-        if (flag) {
-            // datastr.clear();
-            // ROS_INFO("%s", ser.available() ? "available" : "not available");
-            datastr += ser.read(ser.available());
-            if (datastr.length() > 0) {
-                cout << datastr << endl;
-                // ROS_INFO("%d",datastr.length());
-                const char *datachar = datastr.c_str();
-                // ROS_INFO("strlen(datachar) %d\n", strlen(datachar));
-                const char *head = mystrstr(datachar, datastr.length(), front_fbk, strlen(front_fbk));
-                // cout<<datastr.length()<<datastr.data()[FEEDBACK_DATA_LENGTH]<<endl;
-                // printf("%s\n",datastr.data()+FEEDBACK_DATA_LENGTH+5);
-                // cout<<"head "<<head<<endl;
-                // ROS_INFO("head %d\n", (int)(head - datachar));
-                // ROS_INFO("head %d\n",(int)(head));
-                if (head != NULL) {
-                    // printf("%s\n",head);
-                    if ((int) (head + FEEDBACK_DATA_LENGTH + 3 + 2 - datachar) <= datastr.length() &&
-                        head[FEEDBACK_DATA_LENGTH + 3 + 0] == back_fbk[0] &&
-                        head[FEEDBACK_DATA_LENGTH + 3 + 1] == back_fbk[1] &&
-                        head[FEEDBACK_DATA_LENGTH + 3 + 2] == back_fbk[2]) {
-                        struct_feedback_data *feedback_ptr = (struct_feedback_data *) (head + 3);
-                        if (feedback_ptr->check_front_fbk == CHECK_FRONT_FBK &&
-                            feedback_ptr->check_back_fbk == CHECK_BACK_FBK) {
-                            ROS_INFO("Valid serial data received.");
-                            ROS_INFO("speed %f %f %f", feedback_ptr->speed_fbk[0],
-                                     feedback_ptr->speed_fbk[1], feedback_ptr->speed_fbk[2]);
-                            ROS_INFO("positon %f %f %f", feedback_ptr->pos_fbk[0],
-                                     feedback_ptr->pos_fbk[1], feedback_ptr->pos_fbk[2]);
-                            ROS_INFO("imu_a %f %f %f", feedback_ptr->a_fbk[0],
-                                     feedback_ptr->a_fbk[1], feedback_ptr->a_fbk[2]);
-                            ROS_INFO("imu_g %f %f %f", feedback_ptr->g_fbk[0],
-                                     feedback_ptr->g_fbk[1], feedback_ptr->g_fbk[2]);
-                            ROS_INFO("imu_yaw %f", feedback_ptr->yaw_fbk);
-                            ROS_INFO("ultra_sound %f %f %f %f %f %f %f %f %f %f %f %f",
-                                     feedback_ptr->ultra_sound_signal_fbk[0],
-                                     feedback_ptr->ultra_sound_signal_fbk[1],
-                                     feedback_ptr->ultra_sound_signal_fbk[2],
-                                     feedback_ptr->ultra_sound_signal_fbk[3],
-                                     feedback_ptr->ultra_sound_signal_fbk[4],
-                                     feedback_ptr->ultra_sound_signal_fbk[5],
-                                     feedback_ptr->ultra_sound_signal_fbk[6],
-                                     feedback_ptr->ultra_sound_signal_fbk[7],
-                                     feedback_ptr->ultra_sound_signal_fbk[8],
-                                     feedback_ptr->ultra_sound_signal_fbk[9],
-                                     feedback_ptr->ultra_sound_signal_fbk[10],
-                                     feedback_ptr->ultra_sound_signal_fbk[11]);
-                            ROS_INFO("voltage %f", feedback_ptr->voltage_fbk);
-                            ROS_INFO("displacement_sensor_voltage %f", feedback_ptr->displacement_sensor_voltage_fbk);
-                            ROS_INFO("charging_status %d", feedback_ptr->charging_status_fbk);
-                            ROS_INFO("infrared %d %d %d %d %d %d",
-                                     feedback_ptr->infrared_fbk[0],
-                                     feedback_ptr->infrared_fbk[1],
-                                     feedback_ptr->infrared_fbk[2],
-                                     feedback_ptr->infrared_fbk[3],
-                                     feedback_ptr->infrared_fbk[4],
-                                     feedback_ptr->infrared_fbk[5]);
-                            infrared_states_msg.data.clear();
-                            for (int i = 0; i < 6; i++) {
-                                infrared_states_msg.data.push_back(feedback_ptr->infrared_fbk[i]);
-                            }
-                            infrared_states_msg.layout.dim.resize(2);
-                            infrared_states_msg.layout.dim[0].label = "infrared sensors";
-                            infrared_states_msg.layout.dim[0].size = (int) 2;
-                            infrared_states_msg.layout.dim[0].stride = (int) 3;
-                            infrared_states_msg.layout.dim[1].label = "infrared transmitters";
-                            infrared_states_msg.layout.dim[1].size = (int) 3;
-                            infrared_states_msg.layout.dim[1].stride = (int) 1;
-                            infrared_states_publisher.publish(infrared_states_msg);
-
-                            ROS_INFO("qr_code %s\n", feedback_ptr->qr_scan_fbk);
-                            // ROS_INFO("qr_code %d %d %d %d %d %d %d %d %d\n", feedback_ptr->qr_scan_fbk[0]
-                            //          , feedback_ptr->qr_scan_fbk[1]
-                            //          , feedback_ptr->qr_scan_fbk[2]
-                            //          , feedback_ptr->qr_scan_fbk[3]
-                            //          , feedback_ptr->qr_scan_fbk[4]
-                            //          , feedback_ptr->qr_scan_fbk[5]
-                            //          , feedback_ptr->qr_scan_fbk[6]
-                            //          , feedback_ptr->qr_scan_fbk[7]
-                            //          , feedback_ptr->qr_scan_fbk[8]);
-                        } else {
-                            ROS_INFO("invalid check int\n");
-                        }
-                        datastr.clear();
-                    } else {
-                        static int cnt_char_error = 0;
-                        cnt_char_error += 1;
-                        ROS_INFO("invalid check char %c %c %c\n", head[FEEDBACK_DATA_LENGTH + 3 + 0],
-                                 head[FEEDBACK_DATA_LENGTH + 3 + 1], head[FEEDBACK_DATA_LENGTH + 3 + 2]);
-                        if (cnt_char_error > 5) {
-                            cnt_char_error = 0;
-                            datastr.clear();
-                        }
-                    }
-                } else {
-                    ROS_INFO("str match failed\n");
-                }
-            }
-        }
-
-        carVelocity = dataUpdater.getCarVel();
-        velx = (float) carVelocity.linear.x;
-        vely = (float) carVelocity.linear.y;
-        vela = (float) carVelocity.angular.z;
-        vx = (float) (velx * 1000.0);
-        vy = (float) (vely * 1000.0);
-        va = (float) (vela * 1000.0);
-        velFilter(vx, vy, va, zeroCount, lastvx, lastvy, lastva); // filt out single zero
-        speed_cmd[0] = vx;
-        speed_cmd[1] = vy;
-        speed_cmd[2] = va;
-        //        genCmd(cmd, vx, vy, -va); // adjust the opsite direction
-        // static int testint=0;
-        // speed_cmd[0] = testint++;
-        string stringSend;
-        try {
-            //            uint8_t *uint8_t_cmd_ptr = (uint8_t *) cmd;
-            //            ser.write(uint8_t_cmd_ptr, COMMAND_SIZE);
-            // ROS_INFO("stringSend.length() %ld",stringSend.length());
-            send_struct_command_serial(ser);
-        }
-        catch (serial::IOException &e) {
-            ROS_INFO("Write error..");
-        }
-
+    while (ros::ok())
+    {
+        int_msg.data = iter++;
+        int_publisher.publish(int_msg);
+        ROS_INFO("testing..");
         ros::spinOnce();
 
         loop_rate.sleep();
